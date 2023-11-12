6c162bd5f0d8f4afe79d37e7b8075f5ddd35a56f
Karandeep Shoker <141450985+kshoker12@users.noreply.github.com>
2023-11-11 23:06:38 -0800

    Update settings.py

--- ProductivityApp/settings.py
+++ ProductivityApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+import django-heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
