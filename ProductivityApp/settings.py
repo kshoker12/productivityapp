91ec5e7de4e1b46a6ab39362bdda2b257cd9af2e
Karandeep Shoker <141450985+kshoker12@users.noreply.github.com>
2023-11-11 23:08:50 -0800

    Update settings.py

--- ProductivityApp/settings.py
+++ ProductivityApp/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django-heroku
-import dj_database_url
+# import django-heroku
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = "/static"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-django_heroku.settings(locals())
+# STATIC_URL = "/static"
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# django_heroku.settings(locals())
